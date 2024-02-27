17c769dfe022422196f77fcf16a75a4c30255f9a
patt-h <patryk.huchla@wp.pl>
2024-02-27 19:26:05 +0100

    Added communication between services

--- inventory-service/src/main/java/com/example/inventoryservice/Repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/example/inventoryservice/Repository/InventoryRepository.java
@@ -3,8 +3,9 @@ package com.example.inventoryservice.Repository;
 import com.example.inventoryservice.Model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
--- inventory-service/src/main/java/com/example/inventoryservice/Service/InventoryService.java
+++ inventory-service/src/main/java/com/example/inventoryservice/Service/InventoryService.java
@@ -1,9 +1,12 @@
 package com.example.inventoryservice.Service;
 
+import com.example.inventoryservice.DTO.InventoryResponse;
 import com.example.inventoryservice.Repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,14 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                )
+                .toList();
     }
 }
